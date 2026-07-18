a75f8a059fab86f1c5e5e20beb7fcc8a74720f39
Matias Araos <mati.urra.94@gmail.com>
2026-07-18 00:43:25 -0400

    Refine SVG styling and adjust text spacing in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
                             <svg
                                 viewBox="0 0 286 73"
                                 fill="none"
-                                className="absolute -left-6 -right-6 -top-4 -bottom-4 w-[125%] h-[100%] translate-y-2"
+                                className="absolute -left-3 -right-5 -top-3 -bottom-3 w-[112%] h-[100%] translate-y-1.5"
                             >
                                 <motion.path
                                     initial={{ pathLength: 0 }}
@@ -57,10 +57,12 @@ const Hero = () => {
                                     d="M142.293 1C106.854 16.8908 6.08202 7.17705 1.23654 43.3756C-2.10604 68.3466 29.5633 73.2652 122.688 71.7518C215.814 70.2384 316.298 70.689 275.761 38.0785C230.14 1.37835 97.0503 24.4575 52.9384 1"
                                     stroke="#ed6f32"
                                     strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 />
                             </svg>
                         </span>
-                        {'     '}   en cada traslado
+                        {'  '}  en cada traslado
                     </h1>
 
                     <p className="font-body text-xl text-dtll-gray max-w-lg leading-relaxed">
